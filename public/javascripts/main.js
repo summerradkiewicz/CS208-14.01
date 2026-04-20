2e3b5600bb367398cd70b07f3686f85febe8c94a
summerradkiewicz <summerradkiewicz@u.boisestate.edu>
2026-04-20 22:55:21 +0000

    Developed a little more of the landing page

---
+++ public/javascripts/main.js
@@ -0,0 +1,7 @@
+
+// let bgImages = document.querySelectorAll('.donut-img');
+// console.log(bgImages.length);
+// // for (i in bgImages) {
+// //     console.log(i);
+// //     i.style.opacity = "0";
+// // }
