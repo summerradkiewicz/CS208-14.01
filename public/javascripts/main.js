c547c2c626ccc58fd1504cfbd01bafcadf6dd2cc
summerradkiewicz <summerradkiewicz@u.boisestate.edu>
2026-04-30 00:51:39 +0000

    got rid of unused file

--- public/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-
-// let bgImages = document.querySelectorAll('.donut-img');
-// console.log(bgImages.length);
-// // for (i in bgImages) {
-// //     console.log(i);
-// //     i.style.opacity = "0";
-// // }
